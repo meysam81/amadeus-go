f6d2abc51e5bedf4c64917f47879bce38187985e
Meysam <MeysamAzad81@yahoo.com>
2019-02-21 14:36:36 +0330

    cmd/cli/consul-cli: added a new client to send requests **after** finding the server in the consul

---
+++ cmd/cli/consul-cli.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"amadeus-go/cmd/cli/utils"
+
+	"fmt"
+
+	consul "github.com/hashicorp/consul/api"
+)
+
+type cli struct {
+	Name string
+	ConsulAgent *consul.Agent
+}
+
+func main() {
+	c, err := consul.NewClient(consul.DefaultConfig())
+	if err != nil {
+		panic(err)
+	}
+
+	client := cli{
+		Name: "amadeus-go",
+		ConsulAgent: c.Agent(),
+	}
+
+	_, resp, err := client.ConsulAgent.AgentHealthServiceByName("amadeus-go")
+	if err != nil {
+		panic(err)
+	}
+
+	grpcAddr := fmt.Sprintf("%s:%v", resp[0].Service.Address, resp[0].Service.Port)
+	utils.SendReq(&grpcAddr)
+}
