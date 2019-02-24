4c79430a1a999ea71ade9a875e8bb58d32943205
Meysam <MeysamAzad81@yahoo.com>
2019-02-24 11:11:18 +0330

    endpoints/endpoint: add new makeEndpoint for the new API

--- pkg/endpoints/endpoint.go
+++ pkg/endpoints/endpoint.go
@@ -5,18 +5,18 @@ import (
 
 	"context"
 	"errors"
-	"os"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
 )
 
 type AmadeusEndpointSet struct {
-	FlightLowFareSearchEndpoint endpoint.Endpoint
+	FlightLowFareSearchEndpoint     endpoint.Endpoint
+	FlightInspirationSearchEndpoint endpoint.Endpoint
 }
 
-func (s AmadeusEndpointSet) FlightLowFareSearch(ctx context.Context, routeData *sv.FlightLowFareSearchRequest) (*sv.FlightLowFareSearchResponse, error) {
-	resp, err := s.FlightLowFareSearchEndpoint(ctx, routeData)
+func (s AmadeusEndpointSet) FlightLowFareSearch(ctx context.Context, request *sv.FlightLowFareSearchRequest) (*sv.FlightLowFareSearchResponse, error) {
+	resp, err := s.FlightLowFareSearchEndpoint(ctx, request)
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +25,33 @@ func (s AmadeusEndpointSet) FlightLowFareSearch(ctx context.Context, routeData *
 	return response, nil
 }
 
-func MakeFlightLowFareSearchEndpoint(srv sv.AmadeusService) endpoint.Endpoint {
+func (s AmadeusEndpointSet) FlightInspirationSearch(ctx context.Context, request *sv.FlightInspirationSearchRequest) (*sv.FlightInspirationSearchResponse, error) {
+	resp, err := s.FlightInspirationSearchEndpoint(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	response := resp.(*sv.FlightInspirationSearchResponse)
+	return response, nil
+}
+
+func NewEndpointSet(srv sv.AmadeusService, logger log.Logger) *AmadeusEndpointSet {
+	var flightLowFareSearchEndpoint endpoint.Endpoint
+	flightLowFareSearchEndpoint = makeFlightLowFareSearchEndpoint(srv)
+	flightLowFareSearchEndpoint = loggingMiddleware(logger)(flightLowFareSearchEndpoint)
+
+	var flightInspirationSearchEndpoint endpoint.Endpoint
+	flightInspirationSearchEndpoint = makeFlightInspirationSearchEndpoint(srv)
+	flightInspirationSearchEndpoint = loggingMiddleware(logger)(flightInspirationSearchEndpoint)
+
+	return &AmadeusEndpointSet{
+		FlightLowFareSearchEndpoint:     flightLowFareSearchEndpoint,
+		FlightInspirationSearchEndpoint: flightInspirationSearchEndpoint,
+	}
+}
+
+// ====================================================================================================
+func makeFlightLowFareSearchEndpoint(srv sv.AmadeusService) endpoint.Endpoint {
 	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
 		req, ok := request.(*sv.FlightLowFareSearchRequest)
 		if !ok {
@@ -36,17 +62,13 @@ func MakeFlightLowFareSearchEndpoint(srv sv.AmadeusService) endpoint.Endpoint {
 	}
 }
 
-func NewEndpointSet(srv sv.AmadeusService) AmadeusEndpointSet {
-	var logger log.Logger
-	logger = log.NewLogfmtLogger(os.Stderr)
-	logger = log.With(logger, "caller", log.DefaultCaller)
-
-	var flightLowFareSearchEndpoint endpoint.Endpoint
-	flightLowFareSearchEndpoint = MakeFlightLowFareSearchEndpoint(srv)
-
-	flightLowFareSearchEndpoint = loggingMiddleware(logger)(flightLowFareSearchEndpoint)
-
-	return AmadeusEndpointSet{
-		FlightLowFareSearchEndpoint: flightLowFareSearchEndpoint,
+func makeFlightInspirationSearchEndpoint(srv sv.AmadeusService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req, ok := request.(*sv.FlightInspirationSearchRequest)
+		if !ok {
+			return nil, errors.New("bad request")
+		}
+		resp, err := srv.FlightInspirationSearch(ctx, req)
+		return resp, err
 	}
 }
